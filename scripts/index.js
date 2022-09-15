c9fbc866e716bb0c38f5f6af56759052daa1ad81
Prajakta-20 <prajaktav207@gmail.com>
2022-09-15 22:24:00 +0530

    Completed Tasky Application

--- scripts/index.js
+++ scripts/index.js
@@ -10,16 +10,19 @@ const htmlTaskContent = ({ id, title, description, type, url }) => `
     <div class='col-md-6 col-lg-4 mt-3' id=${id} key=${id}>
         <div class='card shadow-sm task__card'>
             <div class='card-header d-flex gap-2 justify-content-end task__card__header'>
-                <button type='button' class='btn btn-outline-info mr-2' name=${id}>
+                <button type='button' class='btn btn-outline-info mr-2' name=${id} onclick="editTask.apply(this, arguments)">
                     <i class='fas fa-pencil-alt' name=${id}></i>
                 </button>
-                <button type='button' class='btn btn-outline-danger mr-2' name=${id}>
+                <button type='button' class='btn btn-outline-danger mr-2' name=${id} onclick="deleteTask.apply(this, arguments)">
                     <i class='fas fa-trash-alt' name=${id}></i>
                 </button>
             </div>
 
             <div class='card-body'>
-                ${url && `<img width='100%' src=${url} alt='card image caption' class='card-image-top md-3 rounded-lg' />`}
+                ${url
+        ? `<img width='100%' height="150px" style="object-fit: cover; object-position: center" src=${url} alt='card image caption' class='card-image-top md-3 rounded-lg' />`
+        : `<img width='100%' height="150px" style="object-fit: cover; object-position: center" src="https://newhorizon-information-science-engineering.s3.ap-south-1.amazonaws.com/nhengineering/information-science-engineering/wp-content/uploads/2020/01/17094526/default-image.png" alt='card image caption' class='card-image-top md-3 rounded-lg' />`
+    }
                 <h4 class='task__card__title'>${title}</h4>
                 <p class='description trim-3-lines text-muted' data-gram_editor='false'>
                     ${description}
@@ -30,13 +33,13 @@ const htmlTaskContent = ({ id, title, description, type, url }) => `
                         ${type}
                     </span>
                 </div>
+            </div>
 
-                <div class='card-footer'>
+            <div class='card-footer'>
                     <button type='button' class='btn btn-outline-primary float-right' data-bs-toggle='modal' data-bs-target='#showTask' id=${id} onclick='openTask.apply(this, arguments)' >
                         Open Task
                     </button>
                 </div>
-            </div>
         </div>
     </div> 
 `;
@@ -101,3 +104,110 @@ const openTask = (e) => {
     const getTask = state.taskList.find(({ id }) => id === e.target.id);
     taskModal.innerHTML = htmlModalContent(getTask);
 }
+
+const deleteTask = (e) => {
+    if (!e) e = window.event;
+
+    const targetID = e.target.getAttribute("name");
+    const type = e.target.tagName;
+    const removeTask = state.taskList.filter(({ id }) => id !== targetID);
+    // console.log(removeTask);
+    state.taskList = removeTask;
+
+    updateLocalStorage();
+    if (type === "BUTTON") {
+        return e.target.parentNode.parentNode.parentNode.parentNode.removeChild(e.target.parentNode.parentNode.parentNode);
+    }
+    return e.target.parentNode.parentNode.parentNode.parentNode.parentNode.removeChild(e.target.parentNode.parentNode.parentNode.parentNode);
+};
+
+const editTask = (e) => {
+    if (!e) e = window.event;
+
+    const targetID = e.target.id;
+    const type = e.target.tagName;
+
+    let parentNode;
+    let taskTitle;
+    let taskDescription;
+    let taskType;
+    let submitButton;
+
+    if (type === "BUTTON") {
+        parentNode = e.target.parentNode.parentNode;
+    } else {
+        parentNode = e.target.parentNode.parentNode.parentNode;
+    }
+
+    taskTitle = parentNode.childNodes[3].childNodes[3];
+    taskDescription = parentNode.childNodes[3].childNodes[5];
+    taskType = parentNode.childNodes[3].childNodes[7].childNodes[1];
+    submitButton = parentNode.childNodes[5].childNodes[1];
+    // console.log(taskTitle, taskDescription, taskType);
+
+    taskTitle.setAttribute("contenteditable", "true");
+    taskDescription.setAttribute("contenteditable", "true");
+    taskType.setAttribute("contenteditable", "true");
+
+    submitButton.setAttribute("onclick", "saveEdit.apply(this, arguments)");
+    submitButton.removeAttribute('data-bs-toggle');
+    submitButton.removeAttribute('data-bs-target');
+    submitButton.innerHTML = "Save Changes";
+};
+
+const saveEdit = (e) => {
+    if (!e) e = window.event;
+
+    const targetID = e.target.id;
+    const parentNode = e.target.parentNode.parentNode;
+    // console.log(parentNode.childNodes);
+
+    const taskTitle = parentNode.childNodes[3].childNodes[3];
+    const taskDescription = parentNode.childNodes[3].childNodes[5];
+    const taskType = parentNode.childNodes[3].childNodes[7].childNodes[1];
+    const submitButton = parentNode.childNodes[5].childNodes[1];
+
+    const updateData = {
+        taskTitle: taskTitle.innerHTML,
+        taskDescription: taskDescription.innerHTML,
+        taskType: taskType.innerHTML,
+    };
+
+    let stateCopy = state.taskList;
+
+    stateCopy = stateCopy.map((task) => task.id === targetID ? {
+        id: task.id,
+        title: updateData.taskTitle,
+        description: updateData.taskDescription,
+        type: updateData.taskType,
+        url: task.url,
+    } : task);
+
+    state.taskList = stateCopy;
+    updateLocalStorage();
+
+    taskTitle.setAttribute("contenteditable", "false");
+    taskDescription.setAttribute("contenteditable", "false");
+    taskType.setAttribute("contenteditable", "false");
+
+    submitButton.setAttribute("onclick", "openTask.apply(this, arguments)");
+    submitButton.setAttribute("data-bs-toggle", "modal");
+    submitButton.setAttribute("data-bs-target", "#showTask");
+    submitButton.innerHTML = "Open Task";
+};
+
+const searchTask = (e) => {
+    if (!e) e = window.event;
+
+    while (taskContents.firstChild) {
+        taskContents.removeChild(taskContents.firstChild);
+    }
+
+    const resultData = state.taskList.filter(({ title }) => {
+        return title.toLowerCase().includes(e.target.value.toLowerCase());
+    });
+
+    resultData.map((cardData) => {
+        taskContents.insertAdjacentHTML("beforeend", htmlTaskContent(cardData));
+    });
+};
